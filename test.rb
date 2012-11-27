39b17e9b84722b6c9142d5d0a2dbff528e99cf26
Colin Pilloud <colin.pilloud@gmail.com>
2012-11-27 13:05:00 -0700

    test script plays mary had a little lamb

--- test.rb
+++ test.rb
@@ -4,14 +4,54 @@ require 'ffi-portaudio'
 require 'radspberry'
 include DSP
 
-puts "starting"
+def play_note f, duration = 0.5
+  Speaker.synth.freq = f
+  sleep duration
+end
+
+def rest(time)
+  Speaker.mute
+  sleep time
+  Speaker.unmute
+end
+
 Speaker[ Phasor.new ]
-sleep 1
-puts "changing frequency"
-Speaker.synth.freq /= 2
-sleep 1
-puts "changing frequency"
-Speaker.synth.freq /= 2
-sleep 1
-puts "muting"
-Speaker.mute
+play_note 329.63
+play_note 293.66
+play_note 261.63
+play_note 293.66
+play_note 329.63
+rest 0.05
+play_note 329.63
+rest 0.05
+play_note 329.63
+rest 0.5
+play_note 293.66
+rest 0.05
+play_note 293.66
+rest 0.05
+play_note 293.66
+rest 0.5
+play_note 329.63
+rest 0.05
+play_note 392.00
+rest 0.05
+play_note 392.00
+rest 0.5
+play_note 329.63
+play_note 293.66
+play_note 261.63
+play_note 293.66
+play_note 329.63
+rest 0.05
+play_note 329.63
+rest 0.05
+play_note 329.63
+rest 0.05
+play_note 329.63
+play_note 293.66
+rest 0.05
+play_note 293.66
+play_note 329.63
+play_note 293.66
+play_note 261.63, 1.5
